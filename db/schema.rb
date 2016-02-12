b8266139a36959365ac19ae60c503c167790961e
hlopezm <hlopezm23@gmail.com>
2016-02-11 21:29:11 -0600

    adding events

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160211044331) do
+ActiveRecord::Schema.define(version: 20160212024420) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "events", force: :cascade do |t|
+    t.string   "artist"
+    t.text     "description"
+    t.decimal  "price_low"
+    t.decimal  "price_high"
+    t.date     "event_date"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+  end
 
   create_table "tickets", force: :cascade do |t|
     t.string   "name"
@@ -24,6 +34,7 @@ ActiveRecord::Schema.define(version: 20160211044331) do
     t.string   "email_address"
     t.datetime "created_at",    null: false
     t.datetime "updated_at",    null: false
+    t.string   "phone"
   end
 
 end
